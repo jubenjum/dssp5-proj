74d559e3f3b8e0746421a333a2b26518ac2ab88f
Juan Benjumea <jubenjum@gmail.com>
2017-03-08 16:20:38 +0100

    new google json and changes in sites class

--- dspr/people.py
+++ dspr/people.py
@@ -104,39 +104,72 @@ class People(Sites):
         self.people_param['pandas_data'] = read_gowalla(self.people_param['data_file'])
 
 
+    def save_people_signatures(self, people_signatures_file):
+        ''' save peoples signature in a pickle '''
+        self.people_param['people_signatures'].to_pickle(people_signatures_file)
+
+
     def compute_path_signature(self):
         '''for each user compute the path signature that is the sum of spot signatures 
         where the pearson has been'''
 
-        def u_func(user):
-            '''just try to paralilize this part of the code ... '''
-            user_signatures = np.zeros((1, self.people_param['d']), dtype=int)
-            for lat_, lon_ in self.people_param['pandas_data']\
-                [self.people_param['pandas_data']['user_id'] == user][['lat','lon']].as_matrix():
-                _sig = np.array([x for x in self.sites_find_closer_site(lat_, lon_, 5)\
-                                                       ['spot_signature']], dtype=int)
-                user_signatures = np.append(user_signatures, _sig, axis=0)
-            user_signature = np.sum(user_signatures, axis=0)
-	    df_user = pd.DataFrame([[user, user_signature]],
-			columns=('user_id','user_signature'))
-            self.df = self.df.append(df_user, ignore_index=True) 
-            print '{} = {}'.format(user, user_signature)
+        ####def u_func(user):
+        ####    '''just try to paralilize this part of the code ... '''
+        ####    user_signatures = np.zeros((1, self.people_param['d']), dtype=int)
+        ####    for lat_, lon_ in self.people_param['pandas_data']\
+        ####        [self.people_param['pandas_data']['user_id'] == user][['lat','lon']].as_matrix():
+        ####        _sig = np.array([x for x in self.sites_find_closer_site(lat_, lon_, 5)\
+        ####                                               ['spot_signature']], dtype=int)
+        ####        user_signatures = np.append(user_signatures, _sig, axis=0)
+        ####    user_signature = np.sum(user_signatures, axis=0)
+	####    df_user = pd.DataFrame([[user, user_signature]],
+	####		columns=('user_id','user_signature'))
+        ####    self.df = self.df.append(df_user, ignore_index=True) 
+        ####    print '{} = {}'.format(user, user_signature)
 
-        self.df = pd.DataFrame(columns=('user_id','user_signature'))
+        ####self.df = pd.DataFrame(columns=('user_id','user_signature'))
+        ####
+	####### multithread ... user_id can be non integer?
+	######users_ = [ int(x) for x in self.people_param['pandas_data']['user_id'].unique()]
+	######with Parallel(n_jobs=2) as parallel:
+	######    parallel(delayed(u_func)(u) for u in users_) 
+
+	##### single thread
+	####for user in self.people_param['pandas_data']['user_id'].unique():
+	####    u_func(user)
+
+        ####self.people_param['people_signatures'] = self.df
         
-	# multithread ... user_id can be non integer?
-	users_ = [ int(x) for x in self.people_param['pandas_data']['user_id'].unique()]
-	Parallel(n_jobs=2)(delayed(u_func)(u) for u in users_)
-        
-	# # single thread
-	# for user in self.people_param['pandas_data']['user_id'].unique():
-	#     u_func(user)
+	self.people_param['people_signatures'] = _compute_path_signature(self.people_param['pandas_data'], \
+	         self.people_param['d'], self.sites_find_closer_site)
 
-        self.people_param['people_signatures'] = self.df
 
-    def save_people_signatures(self, people_signatures_file):
-        ''' save peoples signature in a pickle '''
-        self.people_param['people_signatures'].to_pickle(people_signatures_file)
+def u_func(user, pandas_data, d, search_func):
+    '''just try to paralilize this part of the code ... '''
+    user_signatures = np.zeros((1, d), dtype=int)
+    for lat_, lon_ in pandas_data[pandas_data['user_id'] == user][['lat','lon']].as_matrix():
+        _sig = np.array([x for x in search_func(lat_, lon_, 5)['spot_signature']], dtype=int)
+        user_signatures = np.append(user_signatures, _sig, axis=0)
+    user_signature = np.sum(user_signatures, axis=0)
+    df_user = pd.DataFrame([[user, user_signature]], columns=('user_id','user_signature'))
+    print '{} = {}'.format(user, user_signature)
+    return df_user
+
+
+def _compute_path_signature(pandas_data, d, search_func):
+    ''' '''
+    df = pd.DataFrame(columns=('user_id','user_signature'))
+    
+    users_ = [ int(x) for x in pandas_data['user_id'].unique()]
+    with Parallel(n_jobs=2) as parallel:
+       _r = parallel(delayed(u_func)(u) for u in users_)
+       df = df.append(_r, ignore_index=True)
+
+    #for user in pandas_data['user_id'].unique():
+    #   _r = u_func(user, pandas_data, d, search_func)
+    #   df = df.append(_r, ignore_index=True) 
+
+    return df 
 
 
 if __name__ == '__main__':
