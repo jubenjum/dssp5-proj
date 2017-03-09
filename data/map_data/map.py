a22303edef4c59fa1c1fe303f8a57bac6e733446
Juan Benjumea <jubenjum@gmail.com>
2017-03-09 23:32:45 +0100

    mods in maps

--- data/map_data/map.py
+++ data/map_data/map.py
@@ -6,36 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-axes = plt.gca()
-
-# read and plot all arrondisements
 for file_ in glob.glob('a*.xy'):
-    lats = list(); lons = list()
-    with open(file_) as f:
-        for lines in f.readlines():
-            lat, lon = lines.strip().split(",")
-            lats.append(float(lat))
-            lons.append(float(lon))
-        plt.plot(lons, lats, '-k')
+    xy = np.genfromtxt(file_, delimiter=',')
+    plt.plot(xy[:,0], xy[:,1], '-k')
 
 # get and plot the Seine
 for file_ in glob.glob('seine*.xy'):
-    lats = list(); lons = list()
-    with open(file_) as f:
-        for lines in f.readlines():
-            lat, lon = lines.strip().split(",")
-            lats.append(float(lat))
-            lons.append(float(lon))
-        plt.plot(lons, lats, '-b')
+    xy = np.genfromtxt(file_, delimiter=',')
+    plt.plot(xy[:,0], xy[:,1], color="#6495ED")
 
 # Plot a point for example Eiffel Tower
 plt.plot(2.2945, 48.8584, '*r', markersize=20) # see documentation for more markers
 
-
 # set  other parameters and show the figure
-axes.set_xlim([2.234, 2.454])
-axes.set_ylim([48.801,48.917])
-plt.title('Paris')
+#plt.axis('equal')
+plt.axes().set_xlim([2.25, 2.45])
+plt.axes().set_ylim([48.8,48.9])
+
+plt.title('Paris', fontsize=20)
 plt.show()
 
 
